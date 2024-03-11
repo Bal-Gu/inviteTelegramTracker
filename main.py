a88ae7a29e2adc7b48dea3aff77ca6111d3da5b7
BalGu <guillaume@ballinger.lu>
2024-03-11 14:47:19 +0100

    update bot

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ from telebot.util import update_types
 api_key = "6329679818:AAFSyk6qEc-Gut9sIS7kIBC5rQ4lgXofGEI"
 bot = telebot.TeleBot(token=api_key)
 ref_data = dict()
-channel = "-4195083055"
-final_channel = "-1002035347127"
+channel = "-1001868675721"
+final_channel = "-1002003338835"
 file_path = "ref.json"
+next_update = time.time()
 """
 tg_id = {
 link = "invite_link"
@@ -41,15 +42,20 @@ def save_dict_to_json():
 
 @bot.chat_member_handler()
 def chat_member_update(cmu: types.ChatMemberUpdated):
+    global  next_update
     if cmu.invite_link is None:
         return
     if cmu.from_user.id in ref_data["invited"]:
         return
     for i in ref_data.keys():
+        if i == "invited":
+            continue
         if cmu.invite_link.invite_link == ref_data[i]['link']:
             ref_data[i]["accepted"].append(cmu.from_user.id)
             ref_data[i]["verified"] += 1
+            next_update = time.time() + 600
     ref_data["invited"].append(cmu.from_user.id)
+
 
 @bot.message_handler(commands=["start"])
 def handle_start(message: Message):
@@ -73,6 +79,9 @@ Make sure that the person stays in the channel in order to count.
 
 
 def verify_users():
+    global next_update
+    if next_update < time.time():
+        return
     for i in ref_data.keys():
         if i == "invited":
             continue
@@ -84,7 +93,8 @@ def verify_users():
                     if chat_member.is_member:
                         continue
                     else:
-                        ref_data[i]["accepted"].pop(tg_id)
+                        ref_data[i]["accepted"].remove(tg_id)
+                        ref_data[i]["verified"] -= 1
                 except Exception as e:
                     print(e)
                     time.sleep(3)
@@ -95,11 +105,13 @@ def handle_ranking(message: Message):
     to_send = ""
     unsorted = []
     for i in ref_data.keys():
+        if i == "invited":
+            continue
         unsorted.append((i, ref_data[i]["verified"]))
     sort = sorted(unsorted, key=lambda tup: tup[1], reverse=True)
     rank = 1
     for i in sort:
-        to_send += "{}° @{} ==>\t{} invitation \n".format(rank, ref_data[i[0]]["username"], i[1])
+        to_send += "{}° @{} ==>\t{} invitation \n".format(rank, str(ref_data[i[0]]["username"]).replace("_","\_"), i[1])
         rank += 1
     if to_send == "":
         bot.reply_to(message, "No participation yet. Check back later")
@@ -112,7 +124,7 @@ ref_data = load_dict_from_json()
 scheduler = BackgroundScheduler()
 # Schedule the function to run every minute
 scheduler.add_job(save_dict_to_json, 'interval', minutes=1)
-scheduler.add_job(verify_users, "interval", minutes=10)
+scheduler.add_job(verify_users, "interval", minutes=1)
 scheduler.start()
 
 while True:
